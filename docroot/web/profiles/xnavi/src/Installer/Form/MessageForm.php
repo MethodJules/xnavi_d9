9799ed632b5b5e7b5479bb251ad557c9449403bd
Julien Hofer <hoferj@uni-hildesheim.de>
2021-12-15 17:34:47 +0100

    Updated drupal core to version 9.3.x

--- docroot/web/profiles/xnavi/src/Installer/Form/MessageForm.php
+++ docroot/web/profiles/xnavi/src/Installer/Form/MessageForm.php
@@ -3,6 +3,7 @@
 namespace Drupal\xnavi\Installer\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 
 class MessageForm extends FormBase {
     
@@ -29,7 +30,8 @@ class MessageForm extends FormBase {
     }
 
     public function submitForm(array &$form, FormStateInterface $form_state) {
-        
+        $url = Url::fromRoute('entity.node_type.collection');
+        $form_state->setRedirectUrl($url);
     }
 
 }
