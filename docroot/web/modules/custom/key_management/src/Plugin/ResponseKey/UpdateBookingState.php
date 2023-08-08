f7b2e4471ecfdfefe239ea0f0775249d9c0cef8e
MethodJules <julien.hofer@gv.hamburg.de>
2023-08-08 14:27:58 +0200

    Implemented the update of a booking with the date fields

--- docroot/web/modules/custom/key_management/src/Plugin/ResponseKey/UpdateBookingState.php
+++ docroot/web/modules/custom/key_management/src/Plugin/ResponseKey/UpdateBookingState.php
@@ -3,12 +3,16 @@
 namespace Drupal\key_management\Plugin\ResponseKey;
 
 //TODO: I wrote this detailed but maybe some stuff are already done somewhere else
+
+use DateTimeZone;
 use Drupal\Core\Url;
 use Drupal\key_management\Annotation\ResponseKey;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Drupal\node\Entity\Node;
+use Drupal\Core\Datetime\DrupalDateTime;
+
 /**
  * Class Key Availability.
  *
@@ -27,7 +31,17 @@ use Drupal\node\Entity\Node;
  class UpdateBookingState extends ResponseKeyBase {
     public function getPluginResponse() {
         $content = $this->currentRequest->getContent();
-        $data = json_decode($content, TRUE);
+        $request = json_decode($this->currentRequest->getContent());//Reads the request body JSON
+        $data["buchungID"] = $request->buchungId;
+        $data["zustand"] = $request->zustand;
+        $abholungszeit = $request->abholungszeit; 
+        $abholungszeit = new DrupalDateTime($abholungszeit, new DateTimeZone('UTC'));
+        $data["abholungszeit"] = $abholungszeit;
+
+        $abgabezeit = $request->abgabezeit;
+        $abgabezeit = new DrupalDateTime($abgabezeit, new DateTimeZone('UTC'));
+        $data["abgabezeit"] = $abgabezeit;
+        // $data = json_decode($content, TRUE);
 
         if (isset($data["buchungID"]) && isset($data["zustand"])) {
             $buchungID = $data["buchungID"];
@@ -37,35 +51,32 @@ use Drupal\node\Entity\Node;
             $node = Node::load($buchungID);
             if ($node) {
                 // Update the fields of the node
-                $node->set('field_zustand', $zustand);
+                $node->set('field_buchungszustand', $zustand);
 
                 if (isset($data["abholungszeit"])) {
                     $abholungszeit = $data["abholungszeit"];
-                    $node->set('field_abholungszeit', $abholungszeit);
+                    $node->set('field_abholungszeit', $abholungszeit->format('Y-m-d\TH:i:s'));
                 }
                 if (isset($data["abgabezeit"])) {
                     $abgabezeit = $data["abgabezeit"];
-                    $node->set('field_abgabezeit', $abgabezeit);
+                    $node->set('field_abgabezeit', $abgabezeit->format('Y-m-d\TH:i:s'));
                 }
 
                 // Save the updated node
                 $node->save();
-                return new JsonResponse(['message' => 'Record updated successfully.']);
-            }
-            else {
-                return new JsonResponse(['error' => 'Error updating record.']);
+                return ['message' => 'Record updated successfully.'];
             }
         } 
         else {
-            return new JsonResponse(['error' => 'Missing parameters.']);
+            return ['error' => 'Missing parameters.'];
         }
     }
 
     
-    // public static function postProcessResponse(array $responsedata) {
-    //     $responsedata['timestamp'] = time();
-    //     return $responsedata;
-    // }        //I don't need this
+    public static function postProcessResponse(array $responsedata) {
+         $responsedata['timestamp'] = time();
+         return $responsedata;
+    }
 
     public function getCacheTags() {
         return [];
